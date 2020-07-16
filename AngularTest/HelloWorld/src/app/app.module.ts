b135e8b7195f0bb86a874aacb690b33da2ba44de
user <Tatjana.Solovjova@rtu.lv>
2020-07-16 17:15:54 +0300

    20200716_17_15

--- AngularTest/HelloWorld/src/app/app.module.ts
+++ AngularTest/HelloWorld/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { NavigationComponent } from './navigation/navigation.component';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
